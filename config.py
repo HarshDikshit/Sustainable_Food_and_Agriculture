74e593cb260249cdd4c2589f0f13773aebe10847
HarshDikshit <93562022+HarshDikshit@users.noreply.github.com>
2024-10-07 17:01:12 +0530

    commit

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 class Config:
-    # MONGO_URI = "mongodb+srv://sfa:sfa123@cluster0.w3krj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    MONGO_URI = "mongodb://localhost:27017"
+    MONGO_URI = "mongodb+srv://sfa:sfa123@cluster0.w3krj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    # MONGO_URI = "mongodb://localhost:27017"
